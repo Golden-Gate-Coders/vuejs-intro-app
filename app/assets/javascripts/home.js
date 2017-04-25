cff653c1ceaa52d334919d108c1a41f1a65a5a9e
Danish Mughal <mughal.danish@gmail.com>
2017-04-24 20:49:37 -0700

    v-if and multiple input boxes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,15 +3,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
     el: '#home',
     data: {
       reviews: [
-        "Vue.js is the best!",
-        "I love Vue.js!",
-        "Vue.js is waaaaay better than React."
+        {
+          text: "Vue.js is the best!",
+          rating: 5
+        },
+        {
+          text: "I love Vue.js!",
+          rating: 4
+        },
+        {
+          text: "Vue sucks",
+          rating: 4
+        },
+        {
+          text: "Vue.js is waaaaay better than React.",
+          rating: 5
+        }
       ],
-      newReview: ''
+      newReviewText: '',
+      newReviewRating: 0
     },
     methods: {
       addReview: function() {
-        this.reviews.push(this.newReview);
+        var newReview = {
+          text: this.newReviewText,
+          rating: this.newReviewRating
+        }
+
+        this.reviews.push(newReview);
+        this.newReviewText = '';
+        this.newReviewRating = 0;
+      },
+      isPositive: function(rating) {
+        if (rating >= 3) {
+          return true;
+        } else {
+          return false;
+        }
       }
     }
   });
