998d860960032c5674b2f4cd0d2825a5ddaaa94f
Danish Mughal <mughal.danish@gmail.com>
2017-04-24 20:16:20 -0700

    Adding, removing tasks

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -6,7 +6,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
         "Take out the garbage",
         "Make the bed",
         "Mow the lawn"
-      ]
+      ],
+      newTask: ''
+    },
+    methods: {
+      addTask: function() {
+        if (this.newTask != '') {          
+          this.tasks.push(this.newTask);
+          this.newTask = '';
+        }
+      },
+      removeTask: function(task) {
+        var index = this.tasks.indexOf(task);
+        this.tasks.splice(index, 1);
+      }
     }
   });
 });
